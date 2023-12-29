00b4f952ce813ab9ac31347bf03726ef1c86628a
Chowdhury Adnan Mahmud <138561533+chowdhurycell@users.noreply.github.com>
2023-12-30 00:44:32 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+  const calendar = document.getElementById('calendar');
+  const months = ['SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER']; // Add more months as needed
+
+  months.forEach(month => {
+    const monthContainer = document.createElement('div');
+    monthContainer.classList.add('month');
+
+    // Create the dates-grid container and add it to the month container
+    const datesGrid = document.createElement('div');
+    datesGrid.classList.add('dates-grid');
+    monthContainer.appendChild(datesGrid);
+
+    const totalDays = 31; // Change this based on the month
+
+    for (let day = 1; day <= totalDays; day++) {
+      const dateElement = document.createElement('div');
+      dateElement.classList.add('date');
+
+      // Check if an image should be displayed on this date (modify the condition as needed)
+      const shouldDisplayImage = day === 1 || day === 15 || day === 30;
+
+      if (shouldDisplayImage) {
+        const image = document.createElement('img');
+        // Assuming images are named in the format "month_day.jpg" (e.g., January_1.jpg)
+        image.src = `images/${month}_${day}.jpg`;
+        image.alt = `${month} ${day}`;
+        dateElement.appendChild(image);
+      }
+
+      dateElement.addEventListener('click', function () {
+        alert(`Clicked on ${month} ${day}`);
+        // Add logic to display pictures or perform actions on date click
+      });
+
+      // Add the dateElement to the dates-grid container
+      datesGrid.appendChild(dateElement);
+    }
+
+    // Add the monthContainer to the calendar
+    calendar.appendChild(monthContainer);
+  });
+});
